28aa14f791d40700db873743d02852b3bc483d50
Miguel A. Teruel <materuel@dlsi.ua.es>
2026-02-10 06:45:18 +0100

    Implementar calcular_media y mostrar_resumen

--- notas.py
+++ notas.py
@@ -5,13 +5,20 @@ def leer_notas():
     """
     # TODO (Alumno 1): implementar esta función
     return []
-
+    
 def calcular_media(notas):
     """
     Devuelve la media de una lista de notas (floats).
     """
-    # TODO (Alumno 2): implementar esta función
-    return 0.0
+    if len(notas) == 0:
+        return 0.0
+
+    suma = 0.0
+    for n in notas:
+        suma = suma + n
+
+    return suma / len(notas)
+
 
 def mostrar_resumen(notas):
     """
@@ -21,8 +28,28 @@ def mostrar_resumen(notas):
     - Nota mínima
     - Nota media
     """
-    # TODO (Alumno 2): implementar esta función
-    pass
+    if len(notas) == 0:
+        print("No se han introducido notas.")
+        return
+
+    maximo = notas[0]
+    minimo = notas[0]
+
+    i = 0
+    while i < len(notas):
+        if notas[i] > maximo:
+            maximo = notas[i]
+        if notas[i] < minimo:
+            minimo = notas[i]
+        i = i + 1
+
+    media = calcular_media(notas)
+
+    print("Número de notas:", len(notas))
+    print("Nota máxima:", maximo)
+    print("Nota mínima:", minimo)
+    print("Nota media:", round(media, 2))
+
 
 if __name__ == "__main__":
     notas = leer_notas()
