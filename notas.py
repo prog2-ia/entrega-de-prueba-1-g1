e5d99dd974b2e3494604a778bebc745a53ef851a
Miguel A. Teruel <147790945+materuel-ua@users.noreply.github.com>
2026-02-10 06:42:30 +0100

    Añadir esqueleto de notas.py

---
+++ notas.py
@@ -0,0 +1,29 @@
+def leer_notas():
+    """
+    Pide al usuario notas separadas por espacios y devuelve lista de floats.
+    Por ejemplo: 5 7.5 9 10
+    """
+    # TODO (Alumno 1): implementar esta función
+    return []
+
+def calcular_media(notas):
+    """
+    Devuelve la media de una lista de notas (floats).
+    """
+    # TODO (Alumno 2): implementar esta función
+    return 0.0
+
+def mostrar_resumen(notas):
+    """
+    Muestra por pantalla:
+    - Número de notas
+    - Nota máxima
+    - Nota mínima
+    - Nota media
+    """
+    # TODO (Alumno 2): implementar esta función
+    pass
+
+if __name__ == "__main__":
+    notas = leer_notas()
+    mostrar_resumen(notas)
